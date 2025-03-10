fb4dad2e7760c85767c992570e86316e34538580
Rafael <mikudesudesu@vivaldi.net>
2025-03-09 22:10:47 -0300

    - Finishes the basic problem of the assignment.
    When you visit this back, maybe populate the template with the answers.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -5,7 +5,6 @@ require 'erb'
 require 'date'
 
 
-
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
@@ -74,7 +73,33 @@ def peak_hours_calc
   )
   hours_obj = contents.map { |row| row[:regdate] }
   hours_obj.map! { |hour| Time.strptime(hour, '%m/%d/%y %H:%M') }
-  hours_obj.map! { |hour| hour.strftime('%k'.strip)}.tally
+  peak_hours = hours_obj.map { |hour| hour.strftime('%k'.strip) }.tally
+  peak_days = hours_obj.map { |hour| hour.strftime('%d/%m/%y'.strip) }
+  calc_peak_days = peak_days.map { |days| Time.parse(days).wday }.tally
+  result = week_day_subs(calc_peak_days).sort_by { |_, count| -count }
+  print_this = result.sort_by { |_, count| -count }
+  [peak_hours.sort_by { |_, count| -count }, print_this]
+end
+
+def week_day_subs(date_array)
+  date_array.map.with_index do |week_data, _|
+    case week_data[0]
+    when 0
+      ['Sunday', week_data[1]]
+    when 1
+      ['Monday', week_data[1]]
+    when 2
+      ['Tuesday', week_data[1]]
+    when 3
+      ['Wednesday', week_data[1]]
+    when 4
+      ['Thrusday', week_data[1]]
+    when 5
+      ['Friday', week_data[1]]
+    when 6
+      ['Saturday', week_data[1]]
+  end
+  end
 end
 
 p peak_hours_calc
